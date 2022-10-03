32c34151410d73eba44d00a8c1e54c5ab6309b67
iseletkov <iseletkov@gmail.com>
2022-10-03 12:01:55 +0500

    Добавлен класс для представления контрольных точек. Добавлен список оценок студенту. Добавлено формирование карточки студента с таблицей оценок.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,27 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xwpf.usermodel.*;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
+/********************************************************************************************************
+ * Основной класс программы.                                                                            *
+ * @author Селетков И.П. 2022 0926.                                                                     *
+ *******************************************************************************************************/
 public class Main {
-    private static Map<UUID, CStudent> students = new TreeMap<>();
-    private static Map<UUID, CMark> marks = new TreeMap<>();
+    private static final Map<UUID, CStudent> students = new TreeMap<>();
+    private static final Map<UUID, CMark> marks = new TreeMap<>();
+    private static final Map<UUID, CControl> controls = new TreeMap<>();
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+    /****************************************************************************************************
+     * Открытие электронной таблицы с входными данными.                                                 *
+     * @return - рабочая книга с данными.                                                               *
+     ***************************************************************************************************/
     private static XSSFWorkbook openExcel()
     {
         XSSFWorkbook wb = null;
@@ -34,6 +45,11 @@ public class Main {
 
         return wb;
     }
+    /****************************************************************************************************
+     * Загрузка списка студентов из электронной таблицы.                                                *
+     * Результат в карте students.                                                                      *
+     * @param wb - рабочая книга с данными.                                                             *
+     ***************************************************************************************************/
     private static void loadStudents(XSSFWorkbook wb)
     {
         Sheet sheet = wb.getSheetAt(1);
@@ -65,10 +81,51 @@ public class Main {
             students.put(id, st);
         }
     }
+    /****************************************************************************************************
+     * Загрузка информации о контрольных точках из электронной таблицы.                                 *
+     * Результат в карте controls.                                                                      *
+     * @param wb - рабочая книга с данными.                                                             *
+     ***************************************************************************************************/
     private static void loadKT(XSSFWorkbook wb)
     {
+        Sheet sheet = wb.getSheetAt(2);
 
+        Row row;
+        Cell cell;
+        int i;
+        int nRows = sheet.getLastRowNum();
+        String sUUID, name;
+        UUID id;
+        CControl control;
+        LocalDate date;
+        for (i = 0; i < nRows; i++) {
+            row = sheet.getRow(i);
+            if (row == null)
+                continue;
+            if (row.getLastCellNum() < 3)
+                continue;
+            cell = row.getCell(0);
+            sUUID = cell.getStringCellValue();
+            if (sUUID.length() == 0)
+                continue;
+
+            control = new CControl();
+            id = UUID.fromString(sUUID);
+            control.setId(id);
+            cell = row.getCell(1);
+            name = cell.getStringCellValue();
+            control.setName(name);
+            cell = row.getCell(2);
+            date = cell.getLocalDateTimeCellValue().toLocalDate();
+            control.setDate(date);
+            controls.put(id, control);
+        }
     }
+    /****************************************************************************************************
+     * Загрузка информации об оценках студентов из электронной таблицы.                                 *
+     * Результат в карте marks.                                                                         *
+     * @param wb - рабочая книга с данными.                                                             *
+     ***************************************************************************************************/
     private static void loadMark(XSSFWorkbook wb)
     {
         Sheet sheet = wb.getSheetAt(3);
@@ -79,7 +136,7 @@ public class Main {
         int nRows = sheet.getLastRowNum();
         String sUUID;
         UUID id;
-        Double value;
+        double value;
         CMark mark;
         for (i = 0; i < nRows; i++) {
             row = sheet.getRow(i);
@@ -109,6 +166,12 @@ public class Main {
     {
 
     }
+
+    /****************************************************************************************************
+     * Загрузка информации о связях оценки из электронной таблицы.                                      *
+     * Результат в объектах карт students, marks, controls.                                             *
+     * @param wb - рабочая книга с данными.                                                             *
+     ***************************************************************************************************/
     private static void loadMarkRelations(XSSFWorkbook wb)
     {
         Sheet sheet = wb.getSheetAt(3);
@@ -117,10 +180,11 @@ public class Main {
         Cell cell;
         int i;
         int nRows = sheet.getLastRowNum();
-        String sId, sStudentId, sMarkId;
-        UUID id, studentId, markId;
+        String sId, sStudentId, sControlId;
+        UUID id, studentId, controlId;
         CMark mark;
         CStudent student;
+        CControl control;
         for (i = 0; i < nRows; i++) {
             row = sheet.getRow(i);
             if (row == null)
@@ -139,10 +203,27 @@ public class Main {
             sStudentId = cell.getStringCellValue();
             studentId = UUID.fromString(sStudentId);
             student = students.get(studentId);
-            mark.setStudent(student);
+            if (student!=null)
+            {
+                mark.setStudent(student);
+                student.getMarks().add(mark);
+            }
+            cell = row.getCell(2);
+            sControlId = cell.getStringCellValue();
+            controlId = UUID.fromString(sControlId);
+            control = controls.get(controlId);
+            if (control!=null)
+            {
+                mark.setControl(control);
+            }
 
         }
     }
+    /****************************************************************************************************
+     * Первый этап загрузки данных из электронной таблицы - создание объектов.                          *
+     * Результат в картах students, controls, marks.                                                    *
+     * @param wb - рабочая книга с данными.                                                             *
+     ***************************************************************************************************/
     private static void loadStage1(XSSFWorkbook wb)
     {
         loadStudents(wb);
@@ -150,6 +231,11 @@ public class Main {
         loadMark(wb);
         return;
     }
+    /****************************************************************************************************
+     * Второй этап загрузки данных из электронной таблицы - создание связей между объектами.            *
+     * Результат в картах students, controls, marks.                                                    *
+     * @param wb - рабочая книга с данными.                                                             *
+     ***************************************************************************************************/
     private static void loadStage2(XSSFWorkbook wb)
     {
         loadMarkRelations(wb);
@@ -157,6 +243,10 @@ public class Main {
         loadKTRelations(wb);
         return;
     }
+    /****************************************************************************************************
+     * Загрузка данных из электронной таблицы.                                                          *
+     * Результат в картах students, controls, marks.                                                    *
+     ***************************************************************************************************/
     private static void load()
     {
         try (XSSFWorkbook wb = openExcel())
@@ -175,9 +265,11 @@ public class Main {
             return;
         }
     }
-    public static void main(String[] args) {
-        load();
-
+    /****************************************************************************************************
+     * Вывод данных в консоль.                                                                          *
+     ***************************************************************************************************/
+    private static void outData()
+    {
         for (Map.Entry<UUID, CStudent> pair: students.entrySet())
         {
             System.out.println(pair.getValue());
@@ -186,6 +278,152 @@ public class Main {
         for (Map.Entry<UUID, CMark> pair : marks.entrySet())
         {
             System.out.println(pair.getValue());
+        }
+    }
+    /****************************************************************************************************
+     * Создание заголовка для файла-отчёта.                                                             *
+     * @param document - заготовка файла-отчёта.                                                        *
+     * @param student - студент, данные которого выводятся в отчёт.                                     *
+     ***************************************************************************************************/
+    private static void createTitle(
+            XWPFDocument document,
+            CStudent student
+    )
+    {
+        //Создание параграфа
+        XWPFParagraph par = document.createParagraph();
+        //Центрирование параграфа
+        par.setAlignment(ParagraphAlignment.CENTER);
+
+        //Создание куска текста
+        XWPFRun run = par.createRun();
+        //Установка содержимого текста
+        run.setText(student.getName());
+        //Жирность
+        run.setBold(true);
+        //Шрифт
+        run.setFontFamily("Times New Roman");
+        //Размер шрифта
+        run.setFontSize(20);
+        return;
+    }
+    private static void createHeaderCell(
+            XWPFTableRow row,
+            int pos,
+            String text
+    )
+    {
+        XWPFParagraph par;
+        XWPFRun run;
+        XWPFTableCell cell;
+        cell = row.getCell(pos);
+        //cell.setText("Контрольная точка");
+        par = cell.addParagraph();
+        par.setAlignment(ParagraphAlignment.CENTER);
+        par.setVerticalAlignment(TextAlignment.BOTTOM);
+        run = par.createRun();
+        //Установка содержимого текста
+        run.setText(text);
+        //Жирность
+        run.setBold(true);
+        //Шрифт
+        run.setFontFamily("Times New Roman");
+        //Размер шрифта
+        run.setFontSize(14);
+
+        //cell.setWidth("40%");
+    }
+    /****************************************************************************************************
+     * Создание таблицы с оценками в файле-отчёте.                                                      *
+     * @param document - заготовка файла-отчёта.                                                        *
+     * @param student - студент, данные которого выводятся в отчёт.                                     *
+     ***************************************************************************************************/
+    private static void createTable(
+            XWPFDocument document,
+            CStudent student
+    ) {
+        XWPFTable table = document.createTable(1,3);
+        table.setWidth(5*1440);
+
+        XWPFTableRow row = table.getRow(0);
+        createHeaderCell(row, 0, "Контрольная точка");
+        createHeaderCell(row, 1, "Дата");
+        createHeaderCell(row, 2, "Балл");
+        LocalDate date;
+        String sDate;
+        //Создание строк с информацией по оценкам.
+        for (CMark mark:student.getMarks()) {
+            row = table.createRow();
+            row.getCell(0).setText(mark.getControl().getName());
+            date = mark.getControl().getDate();
+            if (date==null)
+                sDate = "";
+            else
+                sDate = date.format(formatter);
+            row.getCell(1).setText(sDate);
+            row.getCell(2).setText(String.format("%4.1f", mark.getValue()));
+        }
+        //Прокраска границ таблицы. Необходимость надо проверять в MS Word.
+        table.setBottomBorder(XWPFTable.XWPFBorderType.SINGLE, 8, 0, "000000");
+        table.setTopBorder(XWPFTable.XWPFBorderType.SINGLE, 8, 0, "000000");
+        table.setLeftBorder(XWPFTable.XWPFBorderType.SINGLE, 8, 0, "000000");
+        table.setRightBorder(XWPFTable.XWPFBorderType.SINGLE, 8, 0, "000000");
+        table.setInsideHBorder(XWPFTable.XWPFBorderType.SINGLE, 8, 0, "000000");
+        table.setInsideVBorder(XWPFTable.XWPFBorderType.SINGLE, 8, 0, "000000");
+    }
+    /****************************************************************************************************
+     * Создание карточки студента в формате электронного документа.                                     *
+     * @param student - студент, данные которого выводятся в отчёт.                                     *
+     ***************************************************************************************************/
+    private static void createReport(CStudent student)
+    {
+        try(XWPFDocument document = new XWPFDocument())
+        {
+            //Заголовок
+            createTitle(document, student);
+            //Таблица с оценками.
+            createTable(document, student);
+
+            //Сохранение информации в файл.
+            File report = new File("output.docx");
+            try(FileOutputStream fos = new FileOutputStream(report))
+            {
+                document.write(fos);
+            }
+            catch(IOException e)
+            {
+                System.out.println("Ошибка при записи файла на диск!");
+                e.printStackTrace();
+            }
+
+        }
+        catch(IOException e)
+        {
+            System.out.println("Ошибка при сохранении данных в файл!");
+            e.printStackTrace();
+        }
+    }
+    /****************************************************************************************************
+     * Основная функция программы.                                                                      *
+     * @param args - параметры вызова программы из консоли.                                             *
+     ***************************************************************************************************/
+    public static void main(String[] args) {
+        //Загрузка
+        load();
+        //Вывод данных в консоль для проверки
+        outData();
+        //Фильтрация данных. Здесь возвращается первый попавшийся.
+        CStudent st;
+        Iterator<CStudent> it = students.values().iterator();
+        if (it.hasNext())
+        {
+            st = it.next();
+            //Построение картоки студента.
+            createReport(st);
+        }
+        else
+        {
+            System.out.println("В справочнике нет студентов!");
         }
     }
 }
